2e4b6e2a2c54575a739772d4ad42a9de8217c556
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-16 20:33:26 +0530

    [Sai Venkat] Add: Adding WeekDay Using SwitchCase

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -10,13 +10,34 @@ namespace BasicPrograms
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("   Calculator  ");
-            Console.WriteLine("Enter the First Number: ");
-            int a=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Second Number: ");
-            int b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose the options \n1.Calculator \n2.Day of the Week"); 
+            int choose=Convert.ToInt32(Console.ReadLine());
 
-            Calculator.operations(a, b);
+            switch(choose)
+            {
+                case 1:
+                    Console.WriteLine("   Calculator  ");
+
+                    Console.WriteLine("Enter the First Number: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the Second Number: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+
+                    Calculator.operations(a, b);
+                    break;
+                case 2:
+                    Console.WriteLine("   Day of the Week Program");
+                    Console.WriteLine("Choose the Number: ");
+                    int dayNumber=Convert.ToInt32(Console.ReadLine());
+
+                    WeekDay.DayOfTheWeek(dayNumber);
+                    break;
+                default:
+                    Console.WriteLine("Choose the Correct Number");
+                    break;
+                    
+
+            }
         }
     }
 }
---
+++ BasicPrograms/WeekDay.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class WeekDay
+    {
+        public static void DayOfTheWeek(int dayNumber)
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("It's Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("It's Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("It's Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("It's Thrusday");
+                    break;
+                case 5:
+                    Console.WriteLine("It's Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("It's Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("It's Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Choose the Number between 1-7 ");
+                    break;
+            }
+        }
+    }
+}
+
