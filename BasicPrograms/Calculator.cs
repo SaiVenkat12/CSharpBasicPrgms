f5d9b8509be1402a1657955504f99d51f4c07130
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-16 20:18:37 +0530

    [Sai Venkat] Add: Adding Calculator Operations

---
+++ BasicPrograms/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class Calculator
+    {
+        public static void operations(int a,int b)
+        {
+            int c;
+            Console.WriteLine("Choose the options \n1.Addition \n2.Subtraction \n3.Multiplication \n4.Division");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    c = a + b;
+                    Console.WriteLine("Sum = " + c);
+                    break;
+                case 2:
+                    c = Math.Abs(a - b);
+                    Console.WriteLine("Diffrence = " + c);
+                    break;
+                case 3:
+                    c = a * b;
+                    Console.WriteLine("Multipication =" + c);
+                    break;
+                case 4:
+                    c = a / b;
+                    Console.WriteLine("Division = " + c);
+                    break;
+                    default:
+                    Console.WriteLine("Choose the Number between 1-4");
+                        break;
+            }
+
+        }
+    }
+}
---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("   Calculator  ");
+            Console.WriteLine("Enter the First Number: ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number: ");
+            int b=Convert.ToInt32(Console.ReadLine());
+
+            Calculator.operations(a, b);
+        }
+    }
+}
